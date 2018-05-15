cb15865eabefcda11181f64c090f2326b6d61353
wasifmiraz <wasifmiraz@mail.com>
2018-05-15 19:32:25 +0600

    updated controller

---
+++ src/components/countdown/digit/digit.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './digit.css'
+
+const Digit = (props) =>
+ {
+     return(
+         <div className="Digit mx-2 text-center my-4" style = { { borderColor:props.color }}>
+         <h1 className="display-2  py-3" style = { { color:props.color }}>
+
+         {props.value < 10 ? `0${props.value}` : props.value}</h1>
+         </div>
+     )
+
+
+ }
+
+ export default Digit
